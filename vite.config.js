3eb52be8766096b2b06c4f22bb2861b3fa05299e
myWorkSite1667 <worksite1667@gmail.com>
2025-08-31 03:13:37 +0600

    asld

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '192.168.0.104',
+    host: '192.168.0.105',
     port: 5173,
   }
 })
