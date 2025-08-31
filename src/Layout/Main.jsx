0f131cc9ca420548d194046458f5cce4e132e853
myWorkSite1667 <worksite1667@gmail.com>
2025-08-31 15:06:44 +0600

    done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -5,7 +5,7 @@ import Footer from '../Footer/Footer';
 
 const Main = () => {
     return (
-        <div className="bg-sky-50 w-full sm:w-11/12 md:w-10/12 lg:w-9/12 mx-auto mt-5 px-2 sm:px-4 md:px-6">
+        <div className="bg-sky-50 sm:w-11/12 md:w-10/12 lg:w-11/12 mx-auto mt-5">
             <Navbar />
             <Outlet />
             <Footer />
